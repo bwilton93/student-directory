2242aee65bbc2f9ebdd4ba4f4a9f0159b1ed8cb2
Barney <B.Wilton1993@gmail.com>
2023-04-10 13:41:45 +0100

    Added save to file functionality

--- directory.rb
+++ directory.rb
@@ -19,6 +19,7 @@ end
 def interactive_menu
   loop do
     print_menu
+    print "Selection: "
     process(gets.chomp)
   end
 end
@@ -26,6 +27,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we will be adding more items
 end
 
@@ -41,6 +43,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -70,6 +74,18 @@ def print_footer
   else
     puts "great student"
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the students array
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
