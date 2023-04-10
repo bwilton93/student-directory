e6f1c3ad3d4f95453cd20d4290b9af4dcd2c7aa3
Barney <B.Wilton1993@gmail.com>
2023-04-10 12:45:41 +0100

    Refactored code further

--- directory.rb
+++ directory.rb
@@ -3,12 +3,8 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create empty array of students
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
     @students << {name: name, cohort: :november}
     print "Now we have #{@students.count} "
     if @students.count > 1
@@ -16,33 +12,14 @@ def input_students
     else
       puts "student"
     end
-    # get another name
     name = gets.chomp
   end
 end 
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print_students_list
-  @students.each_with_index do |student, index|
-    student_number = index + 1
-    puts "#{student_number}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer
-  if @students.length == 0
-    return
-  end
-
-  print "Overall, we have #{@students.count} "
-  if @students.count > 1
-    puts "great students"
-  else
-    puts "great student"
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
@@ -71,10 +48,27 @@ def process(selection)
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  if @students.length == 0
+    return
+  end
+
+  print "Overall, we have #{@students.count} "
+  if @students.count > 1
+    puts "great students"
+  else
+    puts "great student"
   end
 end
 
